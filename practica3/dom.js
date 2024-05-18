d2f88717d8b0eb6f562405cdce53f9a482a1284e
BenjaOrchansky <borchansky@udesa.edu.ar>
2024-05-18 10:20:30 -0300

    segundo ejercicio practica dom

---
+++ practica3/dom.js
@@ -0,0 +1,13 @@
+let bienvenida = 'Bienvenidos a mi sitio'
+alert(bienvenida)
+let confirmacion = '¿Está seguro de querer avanzar?'
+confirm(confirmacion)
+ 
+if (seguro){
+    saludo = 'Qué alegría que quieras continuar tu visita'
+} else {
+    saludo = 'Lamentamos que no quieras continuar tu visita'
+}
+
+nombre = prompt('Ingrese su nombre: ')
+
